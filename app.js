4d0725fda14050e7ceae87cd4c0978265368d5ed
Jonathan Quesada <jonnythan.quesada@gmail.com>
2020-09-29 14:46:42 -0700

    set up HTML & CSS elements

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // import functions and grab DOM elements
-
+const button = document.getElementById('button');
+const results = document.getElementById('results');
+const input = document.getElementById('input');
 // initialize state
 
 // set event listeners to update state and DOM
+
+button.addEventListener('click', () => {
+    alert('you clicked the button');
+});
